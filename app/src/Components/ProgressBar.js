4d33abd2d8a2c094799766297d368a9254a87919
rishabh <rishabhc329@gmail.com>
2020-01-05 14:55:54 +0530

    Finished progress bar and enchanced performance of display screen for large dataSet

--- app/src/Components/ProgressBar.js
+++ app/src/Components/ProgressBar.js
@@ -1,12 +1,48 @@
 import React from 'react';
-import {Text,View,StyleSheet} from 'react-native';
+import {Text,View,StyleSheet,Dimensions,Animated} from 'react-native';
+const { height, width } = Dimensions.get('window');
 
-const progressBar = () => {
+const progressBar = ({data}) => {
+    const {index,length} = data;
+    // console.log(data);
+    const percent = Math.floor((index+1)/length*100);
     return (
-        <View>
-            <Text>Progress Bar</Text>
+        
+        <View style={styles.progress}>
+            <Text style={styles.text}> {percent}% </Text>
+            <Animated.View style={{...styles.percentage , width:percent*0.95*width/100 }} />
         </View>
     );
 }
 
+const styles =StyleSheet.create({
+    percentage:{
+        marginLeft:0.01*width,
+         backgroundColor:'#6200EE',
+         height:0.0025*height,
+        //  borderWidth:1,
+         borderRadius:100,
+         position:'absolute',
+         top:0,
+         left:0
+    },
+    progress:{
+        marginLeft:0.01*width,
+        borderRadius:100,
+        width:0.95*width,
+        height:0.0025*height,
+        position:'absolute',
+        alignItems:'center',
+        top:0,
+        left:0,
+        backgroundColor:'#e9dff0'
+    },
+    text: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        color:'#542675',
+        textAlignVertical:'center',
+        top:-0.07*height
+    }
+})
 export default progressBar;
--- app/src/Screens/DisplayScreen.js
+++ app/src/Screens/DisplayScreen.js
@@ -7,159 +7,190 @@ import StillCharacter from '../Components/StillCharacter';
 import DictonaryMeaning from '../Components/DictonaryMeaning';
 import AnimatedCharacter from '../Components/AnimatedCharacter';
 
+const data = [
+    {
+        'character': "⺀",
+        'svgData': {
+            'strokes': [
+                "M 323 706 Q 325 699 328 694 Q 334 686 367 671 Q 474 619 574 561 Q 600 545 617 543 Q 627 545 631 559 Q 641 576 613 621 Q 575 684 334 717 Q 321 719 323 706 Z",
+                "M 312 541 Q 314 535 316 531 Q 320 524 347 512 Q 455 461 563 397 Q 588 380 606 380 Q 615 382 619 396 Q 629 414 602 457 Q 564 519 321 554 Q 320 555 319 555 Q 310 555 312 541 Z"
+            ],
+            'animatedStrokes': [
+                'M 336 320 L 450 358 L 554 404 L 587 429 L 614 466',
+                'M 317  476 L 347 493 L 455 528 L 543 568 L 578 594 L 602 629'
+            ]
+        },
+        dictonary: "How do I specify the position of B relative to A With iOS Interface Builder and autoconstraints, this can very explicitly be done and is a breeze. Its not so obvious how one might achieve this with React Native."
+    },
+    {
+        'character': "B",
+        'svgData': {
+            'strokes': [
+                "M 323 706 Q 325 699 328 694 Q 334 686 367 671 Q 474 619 574 561 Q 600 545 617 543 Q 627 545 631 559 Q 641 576 613 621 Q 575 684 334 717 Q 321 719 323 706 Z",
+                "M 312 541 Q 314 535 316 531 Q 320 524 347 512 Q 455 461 563 397 Q 588 380 606 380 Q 615 382 619 396 Q 629 414 602 457 Q 564 519 321 554 Q 320 555 319 555 Q 310 555 312 541 Z"
+            ],
+            'animatedStrokes': [
+                'M 336 320 L 450 358 L 554 404 L 587 429 L 614 466',
+                'M 317  476 L 347 493 L 455 528 L 543 568 L 578 594 L 602 629'
+            ]
+        },
+        dictonary: "Screen 2."
+    },
+    {
+        'character': "C",
+        'svgData': {
+            'strokes': [
+                "M 323 706 Q 325 699 328 694 Q 334 686 367 671 Q 474 619 574 561 Q 600 545 617 543 Q 627 545 631 559 Q 641 576 613 621 Q 575 684 334 717 Q 321 719 323 706 Z",
+                "M 312 541 Q 314 535 316 531 Q 320 524 347 512 Q 455 461 563 397 Q 588 380 606 380 Q 615 382 619 396 Q 629 414 602 457 Q 564 519 321 554 Q 320 555 319 555 Q 310 555 312 541 Z"
+            ],
+            'animatedStrokes': [
+                'M 336 320 L 450 358 L 554 404 L 587 429 L 614 466',
+                'M 317  476 L 347 493 L 455 528 L 543 568 L 578 594 L 602 629'
+            ]
+        },
+        dictonary: "Screen 3"
+    },
+    {
+        'character': "D",
+        'svgData': {
+            'strokes': [
+                "M 323 706 Q 325 699 328 694 Q 334 686 367 671 Q 474 619 574 561 Q 600 545 617 543 Q 627 545 631 559 Q 641 576 613 621 Q 575 684 334 717 Q 321 719 323 706 Z",
+                "M 312 541 Q 314 535 316 531 Q 320 524 347 512 Q 455 461 563 397 Q 588 380 606 380 Q 615 382 619 396 Q 629 414 602 457 Q 564 519 321 554 Q 320 555 319 555 Q 310 555 312 541 Z"
+            ],
+            'animatedStrokes': [
+                'M 336 320 L 450 358 L 554 404 L 587 429 L 614 466',
+                'M 317  476 L 347 493 L 455 528 L 543 568 L 578 594 L 602 629'
+            ]
+        },
+        dictonary: "How do I specify the position of B relative to A With iOS Interface Builder and autoconstraints, this can very explicitly be done and is a breeze. Its not so obvious how one might achieve this with React Native."
+    },
+    {
+        'character': "E",
+        'svgData': {
+            'strokes': [
+                "M 323 706 Q 325 699 328 694 Q 334 686 367 671 Q 474 619 574 561 Q 600 545 617 543 Q 627 545 631 559 Q 641 576 613 621 Q 575 684 334 717 Q 321 719 323 706 Z",
+                "M 312 541 Q 314 535 316 531 Q 320 524 347 512 Q 455 461 563 397 Q 588 380 606 380 Q 615 382 619 396 Q 629 414 602 457 Q 564 519 321 554 Q 320 555 319 555 Q 310 555 312 541 Z"
+            ],
+            'animatedStrokes': [
+                'M 336 320 L 450 358 L 554 404 L 587 429 L 614 466',
+                'M 317  476 L 347 493 L 455 528 L 543 568 L 578 594 L 602 629'
+            ]
+        },
+        dictonary: "Screen 2."
+    },
+    {
+        'character': "F",
+        'svgData': {
+            'strokes': [
+                "M 323 706 Q 325 699 328 694 Q 334 686 367 671 Q 474 619 574 561 Q 600 545 617 543 Q 627 545 631 559 Q 641 576 613 621 Q 575 684 334 717 Q 321 719 323 706 Z",
+                "M 312 541 Q 314 535 316 531 Q 320 524 347 512 Q 455 461 563 397 Q 588 380 606 380 Q 615 382 619 396 Q 629 414 602 457 Q 564 519 321 554 Q 320 555 319 555 Q 310 555 312 541 Z"
+            ],
+            'animatedStrokes': [
+                'M 336 320 L 450 358 L 554 404 L 587 429 L 614 466',
+                'M 317  476 L 347 493 L 455 528 L 543 568 L 578 594 L 602 629'
+            ]
+        },
+        dictonary: "Screen 3"
+    },
+]
+
 const { height, width } = Dimensions.get('window');
 const reducer = (state, action) => {
     switch (action.type) {
         case 'switchComponent':
             return { ...state, component: action.payload };
         case 'updateIndex':
-            return state.index + action.payload < 0 || state.index + action.payload >= 3 ? state : { ...state, index: state.index + action.payload }
+            return state.index + action.payload < 0 || state.index + action.payload >= data.length ? state : { ...state, index: state.index + action.payload }
         default:
             return state;
     }
 };
 
 const DisplayScreen = () => {
-
-    const data = [
-        {
-            'character':"⺀",
-            'svgData': {
-                'strokes' : [
-                    "M 323 706 Q 325 699 328 694 Q 334 686 367 671 Q 474 619 574 561 Q 600 545 617 543 Q 627 545 631 559 Q 641 576 613 621 Q 575 684 334 717 Q 321 719 323 706 Z",
-                    "M 312 541 Q 314 535 316 531 Q 320 524 347 512 Q 455 461 563 397 Q 588 380 606 380 Q 615 382 619 396 Q 629 414 602 457 Q 564 519 321 554 Q 320 555 319 555 Q 310 555 312 541 Z"
-                        ],
-                'animatedStrokes' : [
-                    'M 336 320 L 450 358 L 554 404 L 587 429 L 614 466',
-                    'M 317  476 L 347 493 L 455 528 L 543 568 L 578 594 L 602 629'
-                        ]
-            },
-            dictonary: "How do I specify the position of B relative to A With iOS Interface Builder and autoconstraints, this can very explicitly be done and is a breeze. Its not so obvious how one might achieve this with React Native."
-        },
-        {
-            'character':"⺀",
-            'svgData': {
-                'strokes' : [
-                    "M 323 706 Q 325 699 328 694 Q 334 686 367 671 Q 474 619 574 561 Q 600 545 617 543 Q 627 545 631 559 Q 641 576 613 621 Q 575 684 334 717 Q 321 719 323 706 Z",
-                    "M 312 541 Q 314 535 316 531 Q 320 524 347 512 Q 455 461 563 397 Q 588 380 606 380 Q 615 382 619 396 Q 629 414 602 457 Q 564 519 321 554 Q 320 555 319 555 Q 310 555 312 541 Z"
-                        ],
-                'animatedStrokes' : [
-                    'M 336 320 L 450 358 L 554 404 L 587 429 L 614 466',
-                    'M 317  476 L 347 493 L 455 528 L 543 568 L 578 594 L 602 629'
-                        ]
-            },
-            dictonary: "Screen 2."
-        },
-        {
-            'character':"⺀",
-            'svgData': {
-                'strokes' : [
-                    "M 323 706 Q 325 699 328 694 Q 334 686 367 671 Q 474 619 574 561 Q 600 545 617 543 Q 627 545 631 559 Q 641 576 613 621 Q 575 684 334 717 Q 321 719 323 706 Z",
-                    "M 312 541 Q 314 535 316 531 Q 320 524 347 512 Q 455 461 563 397 Q 588 380 606 380 Q 615 382 619 396 Q 629 414 602 457 Q 564 519 321 554 Q 320 555 319 555 Q 310 555 312 541 Z"
-                        ],
-                'animatedStrokes' : [
-                    'M 336 320 L 450 358 L 554 404 L 587 429 L 614 466',
-                    'M 317  476 L 347 493 L 455 528 L 543 568 L 578 594 L 602 629'
-                        ]
-            },
-            dictonary: "Screen 3"
-        },
-    ]
-
     const [state, dispatch] = useReducer(reducer, {
         component: 'StillCharacter',
-        index: 1,
+        index: 0,
     });
     console.log(state.component);
     return (
         <View>
 
-            <SwiperFlatList
-                // autoplay
-                // autoplayDelay={2}
-                // autoplayLoop
-                disableGesture={true}
-                index={state.index}
-                data={data}
-                renderItem={({ item }) => {
-                    return (
-                        <View style={styles.top}>
-                        {/* Top Area [left button , animated/dictonary/still right Button] */}
 
-                            {/* ---------------- Left button Start ----------------*/}
-                                <TouchableOpacity style={[styles.icon]} onPress={
-                                    () => {
-                                        const showComponent = updateComponenet(state.component);
-                                        dispatch({
-                                            type: 'updateIndex',
-                                            payload: -1,
-                                        });
-                                        if(state.index!=0)
-                                        dispatch({
-                                            type: 'switchComponent',
-                                            payload: 'StillCharacter',
-                                        });
-                                    }}
-                                >
-                                    <Entypo name='chevron-thin-left' size={40} />
-                                </TouchableOpacity>
-                            {/* ---------------- Left button End ----------------*/}
+            <View style={styles.top}>
+                {/* Top Area [left button , animated/dictonary/still right Button] */}
 
-                            {/* ----------------  animated/dictonary/still Start ---------------- */}
-                                
-                                <TouchableOpacity style={{flex: 1}}
-                                 onPress={
-                                    () => {
-                                        const showComponent = updateComponenet(state.component);
-                                        dispatch({
-                                            type: 'switchComponent',
-                                            payload: showComponent,
-                                        })
-                                    }
-                                }>
-                                     <View style={{width: width*0.72, height: 0.5*height, alignItems: 'center',justifyContent:'center'}} >
-                                    {state.component == 'StillCharacter' ?
-                                         <StillCharacter data={data[state.index].character}  /> : null}
-                                    {state.component == 'AnimatedCharacter' ?
-                                         <AnimatedCharacter data={{...data[state.index].svgData , wParent:width*0.72,hParent: 0.5*height}} /> : null}
-                                    {state.component == 'DictonaryMeaning' ?
-                                         <DictonaryMeaning data={data[state.index].dictonary} /> : null}</View>
-                                </TouchableOpacity>
+                {/* ---------------- Left button Start ----------------*/}
+                <TouchableOpacity style={[styles.icon]} onPress={
+                    () => {
+                        const showComponent = updateComponenet(state.component);
+                        dispatch({
+                            type: 'updateIndex',
+                            payload: -1,
+                        });
+                        if (state.index != 0)
+                            dispatch({
+                                type: 'switchComponent',
+                                payload: 'StillCharacter',
+                            });
+                    }}
+                >
+                    <Entypo name='chevron-thin-left' size={40} />
+                </TouchableOpacity>
+                {/* ---------------- Left button End ----------------*/}
 
-                            {/* ----------------  animated/dictonary/still End ---------------- */}
+                {/* ----------------  animated/dictonary/still Start ---------------- */}
+
+                <TouchableOpacity style={{ flex: 1 }}
+                    onPress={
+                        () => {
+                            const showComponent = updateComponenet(state.component);
+                            dispatch({
+                                type: 'switchComponent',
+                                payload: showComponent,
+                            })
+                        }
+                    }>
+                    <View style={{ width: width * 0.72, height: 0.5 * height, alignItems: 'center', justifyContent: 'center' }} >
+                        {state.component == 'StillCharacter' ?
+                            <StillCharacter data={data[state.index].character} /> : null}
+                        {state.component == 'AnimatedCharacter' ?
+                            <AnimatedCharacter data={{ ...data[state.index].svgData, wParent: width * 0.72, hParent: 0.5 * height }} /> : null}
+                        {state.component == 'DictonaryMeaning' ?
+                            <DictonaryMeaning data={data[state.index].dictonary} /> : null}</View>
+                </TouchableOpacity>
+
+                {/* ----------------  animated/dictonary/still End ---------------- */}
 
 
 
 
-                            {/* ----------------   Right button Start ---------------- */}
-                                <TouchableOpacity style={[styles.icon]} onPress={
-                                    () => {
-                                        const showComponent = updateComponenet(state.component);
-                                        dispatch({
-                                            type: 'updateIndex',
-                                            payload: 1,
-                                        });
-                                        if(state.index!=2)
-                                        dispatch({
-                                            type: 'switchComponent',
-                                            payload: 'StillCharacter',
-                                        });
-                                    }}
-                                >
-                                    <Entypo name='chevron-thin-right' size={40} />
-                                </TouchableOpacity>
-                            {/* ----------------   Right button End ---------------- */}
-                            
-                        </View>
-                    )
-                }}
-            />
+                {/* ----------------   Right button Start ---------------- */}
+                <TouchableOpacity style={[styles.icon]} onPress={
+                    () => {
+                        const showComponent = updateComponenet(state.component);
+                        dispatch({
+                            type: 'updateIndex',
+                            payload: 1,
+                        });
+                        if (state.index != data.length)
+                            dispatch({
+                                type: 'switchComponent',
+                                payload: 'StillCharacter',
+                            });
+                    }}
+                >
+                    <Entypo name='chevron-thin-right' size={40} />
+                </TouchableOpacity>
+                {/* ----------------   Right button End ---------------- */}
+
+            </View>
 
 
             {/* rectangle containg pingyinaudio component */}
-            <ProgressBar />
-        </View>
-    );
+            <View style={{ top: 0.91 * height, left: 0, position: 'absolute' }}>
+                <ProgressBar data={{ index: state.index, length: data.length }} /></View>
+        </View>)
+
 };
 
 const updateComponenet = (currentComponent) => {
@@ -177,7 +208,7 @@ const styles = StyleSheet.create({
         width: width
     },
     icon: {
-        marginHorizontal: 0.02 * width,        
+        marginHorizontal: 0.02 * width,
     },
     top: {
         alignItems: 'center',
